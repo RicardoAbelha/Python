ef840477da823f86a5f4c8d3ce917af2c2e6548b
Ricardo Luiz Abelha <rlabelha@gmail.com>
2023-04-28 15:13:32 -0300

    Atualizações 2804

--- EstruturaDeRepeticao/11.py
+++ EstruturaDeRepeticao/11.py
@@ -3,7 +3,7 @@ num1 = int(input("Digite o primeiro numero: "))
 num2 = int(input("Digite o segundo numero: "))
 soma = num1 + num2
 valor = 0
-for i in range(num1,num2):
+for i in range(num1,num2 + 1):
     print(i) 
     valor = valor + i
 print(valor)
